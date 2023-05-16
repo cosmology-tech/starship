ef694a7d7a3639737723130680244aefc1d67479
Anmol1696 <anmol1696@gmail.com>
2023-05-16 19:57:52 +0530

    Update js client to be more generic, cleanup functions, get StarshipClient working with current example

--- examples/osmojs/tests/setup.test.js
+++ examples/osmojs/tests/setup.test.js
@@ -1,10 +1,31 @@
-import {describe} from "node:test";
-import { ChainClientRegistry } from '../src/clients.js';
+import { defaultRegistryTypes } from "@cosmjs/stargate";
+import { describe } from "node:test";
+import { getSigningOsmosisClientOptions } from "osmojs";
+import path from "path";
+import { StarshipClient } from '../src/clients.js';
 
+// Global variable to store chain clients for the tests
 export let ChainClients
 
 beforeAll(async () => {
-  ChainClients = await ChainClientRegistry.withChainIds(["osmosis-1", "cosmos-2"]);
+  const configFile = path.join(__dirname, "..", "configs", "config.yaml");
+  
+  // Instantiate osmosis chain client options
+  const {
+    registry,
+    aminoTypes
+  } = getSigningOsmosisClientOptions({
+    defaultRegistryTypes
+  });
+  
+  ChainClients = await StarshipClient.withConfigFile(configFile, {
+    "osmosis-1": {
+      gasPrice: "0.025uosmo",
+      gasAdjustment: 1.5,
+      registry,
+      aminoTypes,
+    },
+  });
 })
 
 describe("Test clients", () => {
