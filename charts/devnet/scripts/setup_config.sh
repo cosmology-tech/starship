afd0999a61b94e7049d300dd3abebb4f70fbd22b
Anmol1696 <anmol1696@gmail.com>
2023-06-15 08:42:41 +0530

    remove dependence on -z in sed, use line specific changes with sed, update grpc address and enable server

--- charts/devnet/scripts/setup_config.sh
+++ charts/devnet/scripts/setup_config.sh
@@ -8,6 +8,7 @@ set -eu
 
 ls $CHAIN_DIR
 
+
 echo "Update config.toml file"
 sed -i -e 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' $CHAIN_DIR/config/config.toml
 sed -i -e 's/timeout_commit = "5s"/timeout_commit = "1s"/g' $CHAIN_DIR/config/config.toml
@@ -15,18 +16,39 @@ sed -i -e 's/timeout_propose = "3s"/timeout_propose = "1s"/g' $CHAIN_DIR/config/
 sed -i -e 's/index_all_keys = false/index_all_keys = true/g' $CHAIN_DIR/config/config.toml
 sed -i -e 's/seeds = ".*"/seeds = ""/g' $CHAIN_DIR/config/config.toml
 
+
 echo "Update client.toml file"
 sed -i -e 's#keyring-backend = "os"#keyring-backend = "test"#g' $CHAIN_DIR/config/client.toml
 sed -i -e 's#output = "text"#output = "json"#g' $CHAIN_DIR/config/client.toml
 sed -i -e 's#broadcast-mode = "sync"#broadcast-mode = "block"#g' $CHAIN_DIR/config/client.toml
 sed -i -e "s#chain-id = \"\"#chain-id = \"$CHAIN_ID\"#g" $CHAIN_DIR/config/client.toml
 
+
 echo "Update app.toml file"
 sed -i -e "s#minimum-gas-prices = \".*\"#minimum-gas-prices = \"0$DENOM\"#g" $CHAIN_DIR/config/app.toml
 sed -i -e "s#pruning = \".*\"#pruning = \"default\"#g" $CHAIN_DIR/config/app.toml
-sed -i -z -e 's/Enable defines if the API server should be enabled.\nenable = false/Enable defines if the API server should be enabled.\nenable = true/g' $CHAIN_DIR/config/app.toml
 sed -i -e 's#enabled-unsafe-cors = false#enabled-unsafe-cors = true#g' $CHAIN_DIR/config/app.toml
 sed -i -e 's#swagger = false#swagger = true#g' $CHAIN_DIR/config/app.toml
+
+function get_next_line_number() {
+  local txt=$1
+  local file=$2
+  local line_number=$(grep -n $txt $file | cut -d: -f1)
+  echo $((line_number + 1))
+}
+
+line_number=$(get_next_line_number "Enable defines if the API server should be enabled." $CHAIN_DIR/config/app.toml)
+sed -i -e "${line_number}s/enable = false/enable = true/g" $CHAIN_DIR/config/app.toml
+
+line_number=$(get_next_line_number "Address defines the API server to listen on." $CHAIN_DIR/config/app.toml)
+sed -i -e "${line_number}s/address = \".*\"/address = \"tcp://0.0.0.0:1317\"/g" $CHAIN_DIR/config/app.toml
+
+line_number=$(get_next_line_number "Enable defines if the gRPC server should be enabled." $CHAIN_DIR/config/app.toml)
+sed -i -e "${line_number}s/enable = false/enable = true/g" $CHAIN_DIR/config/app.toml
+
+line_number=$(get_next_line_number "Address defines the gRPC server address to bind to." $CHAIN_DIR/config/app.toml)
+sed -i -e "${line_number}s/address = \".*\"/address = \"0.0.0.0:9090\"/g" $CHAIN_DIR/config/app.toml
+
 
 echo "Update consensus params in config.toml"
 sed -i -e "s#timeout_propose = \".*\"#timeout_propose = \"$TIMEOUT_PROPOSE\"#g" $CHAIN_DIR/config/config.toml
