8dac0d8c42323f1bb8481edd68e15b825539e920
Anmol1696 <anmol1696@gmail.com>
2023-06-17 20:16:46 +0530

    Add faucet builder docker file, add a bash script to transfer tokens to funds with retry

---
+++ charts/devnet/scripts/transfer_tokens.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ADDRESS="$1"
+DENOM="$2"
+FAUCET_URL="$3"
+
+set -eu
+
+function transfer_token() {
+  status_code=$(curl --header "Content-Type: application/json" \
+    --request POST --write-out %{http_code} --silent \
+    --data '{"denom":"'"$DENOM"'","address":"'"$ADDRESS"'"}' \
+    $FAUCET_URL)
+  return $status_code
+}
+
+echo "Try to send tokens, if failed, wait for 5 seconds and try again"
+max_tries=10
+while [[ max_tries -gt 0 ]]
+do
+  status_code=$(transfer_token)
+  if [[ "$status_code" -eq 200 ]]; then
+    echo "Successfully sent tokens"
+    exit 0
+  fi
+  echo "Failed to send tokens. Sleeping for 10 secs. Tries left $max_tries"
+  ((max_tries--))
+  sleep 5
+done
