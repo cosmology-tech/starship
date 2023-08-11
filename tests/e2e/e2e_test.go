13920b56da88a14da6945512d021d3a9d8e832b3
Anmol1696 <anmol1696@gmail.com>
2023-08-11 19:12:43 +0530

    fix e2e tests, fix go.mod

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -8,10 +8,12 @@ import (
 	"strings"
 	"testing"
 
-	pb "github.com/cosmology-tech/starship/exposer/exposer"
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/stretchr/testify/suite"
 	"go.uber.org/zap"
+	"gopkg.in/yaml.v3"
+
+	pb "github.com/cosmology-tech/starship/exposer/exposer"
 )
 
 var configEnvKey = "TEST_CONFIG_FILE"
