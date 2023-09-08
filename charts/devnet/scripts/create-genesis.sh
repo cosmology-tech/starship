69e44f16f171fcbf8ba4263e528130d538992272
Anmol <anmol1696@gmail.com>
2023-09-08 19:07:25 +0530

    evmos support (#224)
    * add support for evmos, not working
    
    * got evmos working with changes
    
    * add injective support, might have to move to a differnt repo

--- charts/devnet/scripts/create-genesis.sh
+++ charts/devnet/scripts/create-genesis.sh
@@ -7,15 +7,13 @@ CHAIN_BIN="${CHAIN_BIN:=osmosisd}"
 CHAIN_DIR="${CHAIN_DIR:=$HOME/.osmosisd}"
 KEYS_CONFIG="${KEYS_CONFIG:=configs/keys.json}"
 
-set -eu
+set -eux
 
 # Args and vars specific to chains
 ARGS_ADD_GENESIS_ACCOUNT=""
-GENTX_AMT=5000000000
 if [ $CHAIN_BIN == "injectived" ]
 then
   ARGS_ADD_GENESIS_ACCOUNT="${ARGS_ADD_GENESIS_ACCOUNT} --chain-id ${CHAIN_ID}"
-  GENTX_AMT=5000000000000000000000
 fi
 
 # check if the binary has genesis subcommand or not, if not, set CHAIN_GENESIS_CMD to empty
@@ -28,8 +26,14 @@ echo "Adding key...." $(jq -r ".genesis[0].name" $KEYS_CONFIG)
 jq -r ".genesis[0].mnemonic" $KEYS_CONFIG | $CHAIN_BIN keys add $(jq -r ".genesis[0].name" $KEYS_CONFIG) --recover --keyring-backend="test"
 $CHAIN_BIN $CHAIN_GENESIS_CMD add-genesis-account $($CHAIN_BIN keys show -a $(jq -r .genesis[0].name $KEYS_CONFIG) --keyring-backend="test") $COINS --keyring-backend="test" $ARGS_ADD_GENESIS_ACCOUNT
 
+# todo: adding relayer key is a temporary fix for not having faucet
+# Add relayer key to the keyring and self delegate initial coins
+echo "Adding key...." $(jq -r ".relayers[0].name" $KEYS_CONFIG)
+jq -r ".relayers[0].mnemonic" $KEYS_CONFIG | $CHAIN_BIN keys add $(jq -r ".relayers[0].name" $KEYS_CONFIG) --recover --keyring-backend="test"
+$CHAIN_BIN $CHAIN_GENESIS_CMD add-genesis-account $($CHAIN_BIN keys show -a $(jq -r .relayers[0].name $KEYS_CONFIG) --keyring-backend="test") $COINS --keyring-backend="test" $ARGS_ADD_GENESIS_ACCOUNT
+
 echo "Creating gentx..."
-$CHAIN_BIN $CHAIN_GENESIS_CMD gentx $(jq -r ".genesis[0].name" $KEYS_CONFIG) $GENTX_AMT$DENOM --keyring-backend="test" --chain-id $CHAIN_ID
+$CHAIN_BIN $CHAIN_GENESIS_CMD gentx $(jq -r ".genesis[0].name" $KEYS_CONFIG) $(echo $COINS | cut -d ',' -f1) --keyring-backend="test" --chain-id $CHAIN_ID
 
 echo "Output of gentx"
 cat $CHAIN_DIR/config/gentx/*.json | jq
