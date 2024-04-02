ace24df690f7e62ca9118e53b2508aebd3285a21
Anmol1696 <anmol1696@gmail.com>
2024-04-02 17:40:01 +0530

    get multi-relayer setup working with multiple mnemonics

--- charts/devnet/scripts/default/create-genesis.sh
+++ charts/devnet/scripts/default/create-genesis.sh
@@ -28,19 +28,23 @@ echo "Adding key...." $(jq -r ".faucet[0].name" $KEYS_CONFIG)
 jq -r ".faucet[0].mnemonic" $KEYS_CONFIG | $CHAIN_BIN keys add $(jq -r ".faucet[0].name" $KEYS_CONFIG) --recover --keyring-backend="test"
 $CHAIN_BIN $CHAIN_GENESIS_CMD add-genesis-account $($CHAIN_BIN keys show -a $(jq -r .faucet[0].name $KEYS_CONFIG) --keyring-backend="test") $COINS --keyring-backend="test"
 
-for i in $(seq 0 $NUM_RELAYERS);
-do
-  # Add relayer key and delegate tokens
-  RELAYER_KEY_NAME="$(jq -r '.relayers[$i].name' $KEYS_CONFIG)"
-  echo "Adding relayer key.... $RELAYER_KEY_NAME"
-  jq -r ".relayers[$i].mnemonic" $KEYS_CONFIG | $CHAIN_BIN keys add $RELAYER_KEY_NAME --recover --keyring-backend="test"
-  $CHAIN_BIN $CHAIN_GENESIS_CMD add-genesis-account $($CHAIN_BIN keys show -a $RELAYER_KEY_NAME --keyring-backend="test") $COINS --keyring-backend="test"
-  # Add relayer-cli key and delegate tokens
-  RELAYER_CLI_KEY_NAME="$(jq -r '.relayers[$i].name' $KEYS_CONFIG)"
-  echo "Adding relayer-cli key.... $RELAYER_CLI_KEY_NAME"
-  jq -r ".relayers[$i].mnemonic" $KEYS_CONFIG | $CHAIN_BIN keys add $RELAYER_CLI_KEY_NAME --recover --keyring-backend="test"
-  $CHAIN_BIN $CHAIN_GENESIS_CMD add-genesis-account $($CHAIN_BIN keys show -a $RELAYER_CLI_KEY_NAME --keyring-backend="test") $COINS --keyring-backend="test"
-done
+if [[ $FAUCET_ENABLED == "false" && $NUM_RELAYERS -gt "-1" ]];
+then
+  ## Add relayers keys and delegate tokens
+  for i in $(seq 0 $NUM_RELAYERS);
+  do
+    # Add relayer key and delegate tokens
+    RELAYER_KEY_NAME="$(jq -r ".relayers[$i].name" $KEYS_CONFIG)"
+    echo "Adding relayer key.... $RELAYER_KEY_NAME"
+    jq -r ".relayers[$i].mnemonic" $KEYS_CONFIG | $CHAIN_BIN keys add $RELAYER_KEY_NAME --recover --keyring-backend="test"
+    $CHAIN_BIN $CHAIN_GENESIS_CMD add-genesis-account $($CHAIN_BIN keys show -a $RELAYER_KEY_NAME --keyring-backend="test") $COINS --keyring-backend="test"
+    # Add relayer-cli key and delegate tokens
+    RELAYER_CLI_KEY_NAME="$(jq -r ".relayers_cli[$i].name" $KEYS_CONFIG)"
+    echo "Adding relayer-cli key.... $RELAYER_CLI_KEY_NAME"
+    jq -r ".relayers_cli[$i].mnemonic" $KEYS_CONFIG | $CHAIN_BIN keys add $RELAYER_CLI_KEY_NAME --recover --keyring-backend="test"
+    $CHAIN_BIN $CHAIN_GENESIS_CMD add-genesis-account $($CHAIN_BIN keys show -a $RELAYER_CLI_KEY_NAME --keyring-backend="test") $COINS --keyring-backend="test"
+  done
+fi
 
 ## if facuet not enabled then add validator and relayer with index as keys and into gentx
 if [[ $FAUCET_ENABLED == "false" && $NUM_VALIDATORS -gt "1" ]];
