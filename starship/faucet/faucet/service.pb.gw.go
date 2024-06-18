16d8e4892f6e251fa7225c81e9c52e7851199551
Anmol1696 <anmol1696@gmail.com>
2024-06-18 20:37:32 +0530

    fix typo, update proto generated files

--- starship/faucet/faucet/service.pb.gw.go
+++ starship/faucet/faucet/service.pb.gw.go
@@ -138,21 +138,21 @@ func RegisterFaucetHandlerServer(ctx context.Context, mux *runtime.ServeMux, ser
 // RegisterFaucetHandlerFromEndpoint is same as RegisterFaucetHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterFaucetHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.DialContext(ctx, endpoint, opts...)
+	conn, err := grpc.NewClient(endpoint, opts...)
 	if err != nil {
 		return err
 	}
 	defer func() {
 		if err != nil {
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 			return
 		}
 		go func() {
 			<-ctx.Done()
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 		}()
 	}()
