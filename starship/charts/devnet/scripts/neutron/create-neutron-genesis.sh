1eda7fcfd2513453d50f482532ff5b71c88ff97d
Anmol1696 <anmol1696@gmail.com>
2024-07-12 11:37:39 +0530

    hotfix: update neutron setup to be divided into update config

--- starship/charts/devnet/scripts/neutron/create-neutron-genesis.sh
+++ starship/charts/devnet/scripts/neutron/create-neutron-genesis.sh
@@ -14,11 +14,6 @@ FAUCET_ENABLED="${FAUCET_ENABLED:=true}"
 NUM_VALIDATORS="${NUM_VALIDATORS:=1}"
 NUM_RELAYERS="${NUM_RELAYERS:=0}"
 
-P2PPORT=${P2PPORT:-26656}
-RPCPORT=${RPCPORT:-26657}
-RESTPORT=${RESTPORT:-1317}
-ROSETTA=${ROSETTA:-8080}
-
 GENESIS_PATH="$CHAIN_DIR/config/genesis.json"
 
 CONTRACTS_BINARIES_DIR=${CONTRACTS_BINARIES_DIR:-./contracts}
@@ -71,28 +66,6 @@ $BINARY add-genesis-account $($BINARY keys show -a demowallet2 --keyring-backend
 echo "Adding key.... demowallet2"
 jq -r ".keys[0].mnemonic" $KEYS_CONFIG | $BINARY keys add demowallet3 --index 3 --recover --keyring-backend="test"
 $BINARY add-genesis-account $($BINARY keys show -a demowallet3 --keyring-backend="test") $COINS --keyring-backend="test"
-
-sed -i -e 's/timeout_commit = "5s"/timeout_commit = "1s"/g' "$CHAIN_DIR/config/config.toml"
-sed -i -e 's/timeout_propose = "3s"/timeout_propose = "1s"/g' "$CHAIN_DIR/config/config.toml"
-sed -i -e 's/index_all_keys = false/index_all_keys = true/g' "$CHAIN_DIR/config/config.toml"
-sed -i -e 's/enable = false/enable = true/g' "$CHAIN_DIR/config/app.toml"
-sed -i -e 's/swagger = false/swagger = true/g' "$CHAIN_DIR/config/app.toml"
-sed -i -e "s/minimum-gas-prices = \"\"/minimum-gas-prices = \"0$STAKEDENOM\"/g" "$CHAIN_DIR/config/app.toml"
-sed -i -e 's/enabled = false/enabled = true/g' "$CHAIN_DIR/config/app.toml"
-sed -i -e 's/prometheus-retention-time = 0/prometheus-retention-time = 1000/g' "$CHAIN_DIR/config/app.toml"
-sed -i -e 's/enabled-unsafe-cors = false/enabled-unsafe-cors = true/g' "$CHAIN_DIR/config/app.toml"
-
-sed -i -e 's#"tcp://0.0.0.0:26656"#"tcp://0.0.0.0:'"$P2PPORT"'"#g' "$CHAIN_DIR/config/config.toml"
-sed -i -e 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:'"$RPCPORT"'"#g' "$CHAIN_DIR/config/config.toml"
-sed -i -e 's#"tcp://localhost:1317"#"tcp://0.0.0.0:'"$RESTPORT"'"#g' "$CHAIN_DIR/config/app.toml"
-sed -i -e 's#"tcp://0.0.0.0:1317"#"tcp://0.0.0.0:'"$RESTPORT"'"#g' "$CHAIN_DIR/config/app.toml"
-sed -i -e 's#":8080"#":'"$ROSETTA"'"#g' "$CHAIN_DIR/config/app.toml"
-sed -i -e 's#localhost#0.0.0.0#g' "$CHAIN_DIR/config/app.toml"
-
-echo "Update client.toml file"
-sed -i -e 's#keyring-backend = "os"#keyring-backend = "test"#g' $CHAIN_DIR/config/client.toml
-sed -i -e 's#output = "text"#output = "json"#g' $CHAIN_DIR/config/client.toml
-sed -i -e "s#chain-id = \"\"#chain-id = \"$CHAIN_ID\"#g" $CHAIN_DIR/config/client.toml
 
 echo "Update genesis.json file with updated local params"
 sed -i "s/\"time_iota_ms\": \".*\"/\"time_iota_ms\": \"$TIME_IOTA_MS\"/" $CHAIN_DIR/config/genesis.json
