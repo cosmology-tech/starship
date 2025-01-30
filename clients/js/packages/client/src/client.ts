98897eb96b163566ccb7d20408fe151fac0137e3
Anmol1696 <anmol1696@gmail.com>
2025-01-30 14:28:13 +0400

    fixed client versions, update default helm version

--- clients/js/packages/client/src/client.ts
+++ clients/js/packages/client/src/client.ts
@@ -51,7 +51,7 @@ export interface PodPorts {
 }
 
 const defaultName: string = 'starship';
-const defaultVersion: string = 'v0.2.19';
+const defaultVersion: string = 'v1.0.0';
 
 // TODO talk to Anmol about moving these into yaml, if not already possible?
 const defaultPorts: PodPorts = {
